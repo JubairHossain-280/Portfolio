4999ecf9e4a96e43bcd169ac24df4d6309fe4d43
jubairPractise <jubairnpc@gmailcom>
2024-11-09 00:17:21 +0600

    few changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div name='home' className='bg-gradient-to-b from-black via-black to-gray-800 w-full h-screen'>
        <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full md:flex-row w-11/12'>
-        <div className='flex flex-col justify-center mb-8 w-11/12 mx-auto mt-10 md:mb-0 md:mt-0'>
+        <div className='flex flex-col justify-center mb-8 w-full mx-auto md:mb-0'>
             <h2 className='text-4xl md:text-5xl lg:text-7xl font-bold text-white'>
                 I'm a Front-End Developer
             </h2>
