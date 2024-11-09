e7b306be6bd28dc7c1fd753bcfd67533c4e80817
jubairPractise <jubairnpc@gmailcom>
2024-11-09 13:41:35 +0600

    working on responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name='home' className='bg-gradient-to-b from-black via-black to-gray-800 w-full h-screen'>
+    <div name='home' className='bg-gradient-to-b from-black via-black to-gray-800 w-full h-full py-10 md:h-screen md:py-0'>
        <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full md:flex-row w-11/12'>
-        <div className='flex flex-col justify-center mb-8 w-full mx-auto md:mb-0'>
+        <div className='flex flex-col justify-center mb-8 mx-auto md:mb-0'>
             <h2 className='text-4xl md:text-5xl lg:text-7xl font-bold text-white'>
                 I'm a Front-End Developer
             </h2>
