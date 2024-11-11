3fabb027aff2338969b2cecc256981a31e58e8eb
jubairPractise <jubairnpc@gmailcom>
2024-11-11 21:54:10 +0600

    changes in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkColor': '#0D0518',
+        
+      },
+      gradientColorStops: {
+        'darkColor': '#0D0518',
+        'MediumDarkColor': '#170623',
+        'MediumLightColor': '#1D0D2F',
+        'lightColor': '#360748',
+
+      },
+
+    },
     fontFamily: {
       signature: ["Great Vibes"],
     },
